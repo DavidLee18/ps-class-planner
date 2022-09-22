120933747879610ed2d5e9e04ace17bd14ab3017
이재현 <djwodus@gmail.com>
2022-09-23 03:14:27 +0900

    upgrade sass importer

--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,7 @@ module.exports = [
                   // See https://github.com/webpack-contrib/sass-loader/issues/804
                   webpackImporter: false,
                   sassOptions: {
+                    importer: materialImporter,
                     includePaths: ['./node_modules']
                   }
                 }
@@ -65,3 +66,30 @@ module.exports = [
       mode: 'production'
     }
   ];
+
+
+  function tryResolve_(url, sourceFilename) {
+    // Put require.resolve in a try/catch to avoid node-sass failing with cryptic libsass errors
+    // when the importer throws
+    try {
+      return require.resolve(url, {paths: [path.dirname(sourceFilename)]});
+    } catch (e) {
+      return '';
+    }
+  }
+  
+  function tryResolveScss(url, sourceFilename) {
+    // Support omission of .scss and leading _
+    const normalizedUrl = url.endsWith('.scss') ? url : `${url}.scss`;
+    return tryResolve_(normalizedUrl, sourceFilename) ||
+      tryResolve_(path.join(path.dirname(normalizedUrl), `_${path.basename(normalizedUrl)}`),
+        sourceFilename);
+  }
+  
+  function materialImporter(url, prev) {
+    if (url.startsWith('@material')) {
+      const resolved = tryResolveScss(url, prev);
+      return {file: resolved || url};
+    }
+    return {file: url};
+  }
