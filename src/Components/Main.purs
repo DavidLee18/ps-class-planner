ddfd0f1dba571afd0ac13edc145b18a3b49e3fa8
이재현 <djwodus@gmail.com>
2022-09-24 20:22:30 +0900

    testing material-components-web

--- src/Components/Main.purs
+++ src/Components/Main.purs
@@ -2,6 +2,7 @@ module Components.Main where
 
 import Prelude
 
+import Data.Array (replicate)
 import Data.Subject (Subject(..), ex1)
 import Effect.Aff (Aff)
 import Elements.Material (Cell(..), Column(..), dataTable)
@@ -22,4 +23,6 @@ mainComponent = H.mkComponent { eval: H.mkEval $ defaultEval
 render ∷ ∀ w i. State → HTML w i
 render { subject: (Subject s) } = dataTable
     (StringColumn <$> ["구분", "학년", "교과목명", "과목코드", "학점", "과목구분 및 시간", "강좌시간", "정원", "타학과수강 가능여부"])
-    [ StringCell <$> [show s.subjectType, show s.grade, s.name, s.id, show s.point, show s.time, show s.lectureTimes, show s.quota, show s.open] ]
+    (replicate 5 row)
+    where
+      row = StringCell <$> [show s.subjectType, show s.grade, s.name, s.id, show s.point, show s.time, show s.lectureTimes, show s.quota, show s.open]
